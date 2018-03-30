62cd1b537d1544be4f283893c2e8dd672ad531f6
RomeoGolf <romeogolf@none.com>
2018-03-30 10:44:57 +0500

    Форматирование:
    изменено объявление указателей,
    исправлены отступы,
    исправлены первые скобки функций,
    перенесены строки до длины 80

--- Lib/fake_fs.h
+++ Lib/fake_fs.h
@@ -8,18 +8,21 @@
 /* from ..\..\LUFA\CodeTemplates\DriverStubs\Dataflash.h */
     /* Public Interface - May be used in end-application: */
         /* Macros: */
-            /** Constant indicating the total number of dataflash ICs mounted on the selected board. */
-            #define DATAFLASH_TOTALCHIPS     1 // TODO: Replace with the number of Dataflashes on the board, max 2
+            /** Constant indicating the total number of dataflash ICs
+             * mounted on the selected board. */
+            #define DATAFLASH_TOTALCHIPS     1
             /** Mask for no dataflash chip selected. */
             #define DATAFLASH_NO_CHIP        0
             /** Mask for the first dataflash chip selected. */
-            #define DATAFLASH_CHIP1          1 // TODO: Replace with mask with the pin attached to the first Dataflash /CS set
+            #define DATAFLASH_CHIP1          1
             /** Mask for the second dataflash chip selected. */
-            #define DATAFLASH_CHIP2          2 // TODO: Replace with mask with the pin attached to the second Dataflash /CS set
-            /** Internal main memory page size for the board's dataflash ICs. */
-            #define DATAFLASH_PAGE_SIZE      1024 // TODO: Replace with the page size for the Dataflash ICs
-            /** Total number of pages inside each of the board's dataflash ICs. */
-            #define DATAFLASH_PAGES          8192 // TODO: Replace with the total number of pages inside one of the Dataflash ICs
+            #define DATAFLASH_CHIP2          2
+            /** Internal main memory page size for the board's
+             * dataflash ICs. */
+            #define DATAFLASH_PAGE_SIZE      1024
+            /** Total number of pages inside each of the board's
+             * dataflash ICs. */
+            #define DATAFLASH_PAGES          8192
 
 /* from DataflashManager.h */
     /* Preprocessor Checks: */
@@ -28,17 +31,23 @@
         #endif
 
     /* Defines: */
-        /** Total number of bytes of the storage medium, comprised of one or more Dataflash ICs. */
+        /** Total number of bytes of the storage medium, comprised
+         * of one or more Dataflash ICs. */
         #define VIRTUAL_MEMORY_BYTES         ((uint32_t)DATAFLASH_PAGES * DATAFLASH_PAGE_SIZE * DATAFLASH_TOTALCHIPS)
-        /** Block size of the device. This is kept at 512 to remain compatible with the OS despite the underlying
-         *  storage media (Dataflash) using a different native block size. Do not change this value.
+        /** Block size of the device. This is kept at 512 to remain
+         * compatible with the OS despite the underlying
+         * storage media (Dataflash) using a different native block
+         * size. Do not change this value.
          */
         #define VIRTUAL_MEMORY_BLOCK_SIZE    512
-        /** Total number of blocks of the virtual memory for reporting to the host as the device's total capacity. Do not
-         *  change this value; change VIRTUAL_MEMORY_BYTES instead to alter the media size.
+        /** Total number of blocks of the virtual memory for reporting
+         * to the host as the device's total capacity. Do not change
+         * this value; change VIRTUAL_MEMORY_BYTES instead to alter
+         * the media size.
          */
         #define VIRTUAL_MEMORY_BLOCKS        (VIRTUAL_MEMORY_BYTES / VIRTUAL_MEMORY_BLOCK_SIZE)
-        /** Blocks in each LUN, calculated from the total capacity divided by the total number of Logical Units in the device. */
+        /** Blocks in each LUN, calculated from the total capacity
+         * divided by the total number of Logical Units in the device. */
         #define LUN_MEDIA_BLOCKS             (VIRTUAL_MEMORY_BLOCKS / TOTAL_LUNS)
 
     /* Function Prototypes: */
